d41fbf1e681197055aa018c77644f98cf2fe5424
allenjasondc <allenjasondc@gmail.com>
2025-07-01 14:55:29 +0800

    Real time subscriptions

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,11 +1,17 @@
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
 import OrderListItem from "@/components/OrderListItem";
 import { useAdminOrderList } from "@/api/orders";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { useQueryClient } from "@tanstack/react-query";
+import { useInsertOrderSubscription } from "@/api/orders/subscriptions";
 
 export default function OrderScreen() {  
 
   const {data: orders, isLoading, error} = useAdminOrderList({archived: false})
   
+  useInsertOrderSubscription()
+
   if(isLoading){
     return <ActivityIndicator />
   }
